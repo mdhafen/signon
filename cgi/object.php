b6561a4502dd7597bb85cdf21dca84cf98096066
Michael Hafen <michael.hafen@washk12.org>
2014-10-23 14:25:53 -0600

    add pages to show details on an object

---
+++ cgi/object.php
@@ -0,0 +1,47 @@
+<?php
+include_once( '../lib/input.phpm' );
+include_once( '../lib/security.phpm' );
+include_once( '../lib/data.phpm' );
+include_once( '../lib/output.phpm' );
+
+global $config;
+
+do_ldap_connect();
+if ( ! authenticate() ) {
+	output( array(), 'login' );
+	exit;
+}
+
+$dn = input( 'dn', INPUT_STR );
+
+$set = ldap_quick_search( array( 'objectClass' => '*' ), array(), 0, $dn );
+$object = $set[0];
+$objectdn = $object['dn'];
+unset( $object['dn'] );
+
+ksort( $object, SORT_STRING | SORT_FLAG_CASE );
+
+$parentdn = substr( $objectdn, strpos( $objectdn, ',' ) +1 );
+if ( $parentdn == $config['ldap']['base'] ) {
+	$parentdn = '';
+}
+
+$children = ldap_quick_search( array( 'objectClass' => '*' ), array(), 1, $dn );
+usort( $children, 'sorter' );
+
+$output = array(
+	'object_dn' => $objectdn,
+	'object' => $object,
+	'parentdn' => $parentdn,
+	'children' => $children,
+);
+
+output( $output, 'object.tmpl' );
+
+function sorter( $a, $b ) {
+	$av = empty($a['cn']) ? $a['ou'][0] : $a['cn'][0];
+	$bv = empty($b['cn']) ? $b['ou'][0] : $b['cn'][0];
+
+	return strcasecmp( $av, $bv );
+}
+?>
---
+++ htdocs/object.tmpl.php
@@ -0,0 +1,50 @@
+<?php include( 'doc-open.php' ); ?>
+<title>Welcome to the Staff Management page</title>
+<?php
+include( 'doc-head-close.php' );
+include( 'doc-header.php' );
+include( 'doc-menu.php' );
+?>
+
+<h1><?= $data['object_dn'] ?></h1>
+<div>
+<?php foreach ( $data['object'] as $key => $vals ) { ?>
+<div class='object_attribute'><span class="object_attr_key"><?= $key ?></span>
+<?php   foreach ( $vals as $val ) { ?>
+<div class="object_attr_value"><?= $val ?></div>
+<?php   } ?>
+</div>
+<?php } ?>
+</div>
+
+<?php if ( $data['parentdn'] ) { ?>
+<div>
+<a href="object.php?dn=<?= urlencode($data['parentdn']) ?>">Up</a>
+</div>
+<?php } else { ?>
+<div>
+<a href="index.php">Up</a>
+</div>
+<?php } ?>
+
+<?php if ( count( $data['children'] ) ) { ?>
+<h2>Children</h2>
+<div>
+<?php foreach ( $data['children'] as $child ) { ?>
+<div>
+<a href="object.php?dn=<?= urlencode($child['dn']) ?>"><?= empty($child['cn']) ? $child['ou'][0] : $child['cn'][0] ?></a>
+<?php if ( in_array('inetOrgPerson',$child['objectClass']) ) { ?>
+(Person)
+<?php } else if ( in_array('person',$child['objectClass']) ) { ?>
+(System Account)
+<?php } else if ( in_array('groupOfNames',$child['objectClass']) ) { ?>
+(Group)
+<?php } else if ( in_array('organizationalUnit',$child['objectClass']) ) { ?>
+(Folder)
+<?php } ?>
+</div>
+<?php } ?>
+</div>
+<?php } ?>
+
+<?php include( 'doc-close.php' ); ?>
