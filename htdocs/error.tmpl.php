98eb9135024f78d40ae212e3e3bc424220aa9b1c
Michael Hafen <michael.hafen@washk12.org>
2014-11-20 12:17:06 -0700

    Adding error() to lib/output
    This is to output error messages, which are fatal (i.e. the script will end
    there).
    This is in preparation for the save.php script, which will either end in
    error or by redirecting to object.php

---
+++ htdocs/error.tmpl.php
@@ -0,0 +1,18 @@
+<?php include( 'doc-open.php' ); ?>
+<title>Staff Login Management - Error</title>
+<?php
+include( 'doc-head-close.php' );
+include( 'doc-header.php' );
+?>
+
+<h1>An error occured!</h1>
+
+<?php
+foreach ( $data['errors'] as $error ) {
+?>
+<div class='important'><?= $error ?>:</div>
+<?php
+}
+?>
+
+<?php include( 'doc-close.php' ); ?>
