05911ba75b9e6443af2a276cd061eedd6878be3d
Jeremy Cox <jeremy.cox@washk12.org>
2015-02-11 14:59:09 -0700

    Initial Bootstrap implimentation

--- htdocs/doc-head-close.php
+++ htdocs/doc-head-close.php
@@ -1,2 +1,3 @@
+<link rel="stylesheet" type="text/css" href="<?= $data['_config']['base_url'] ?>css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="<?= $data['_config']['base_url'] ?>css/site.css">
 </head>
