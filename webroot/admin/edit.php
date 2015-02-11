b73c2decb18f1110cd9eb2cfbccd8751d4fc049c
Michael Hafen <michael.hafen@washk12.org>
2015-02-11 13:40:58 -0700

    fixing include path after moving files

--- webroot/admin/edit.php
+++ webroot/admin/edit.php
@@ -1,10 +1,10 @@
 <?php
-include_once( '../lib/input.phpm' );
-include_once( '../lib/security.phpm' );
-include_once( '../lib/data.phpm' );
-include_once( '../lib/output.phpm' );
-include_once( '../inc/person.phpm' );
-include_once( '../inc/schema.phpm' );
+include_once( '../../lib/input.phpm' );
+include_once( '../../lib/security.phpm' );
+include_once( '../../lib/data.phpm' );
+include_once( '../../lib/output.phpm' );
+include_once( '../../inc/person.phpm' );
+include_once( '../../inc/schema.phpm' );
 
 global $config;
 
--- webroot/admin/object.php
+++ webroot/admin/object.php
@@ -1,9 +1,9 @@
 <?php
-include_once( '../lib/input.phpm' );
-include_once( '../lib/security.phpm' );
-include_once( '../lib/data.phpm' );
-include_once( '../lib/output.phpm' );
-include_once( '../inc/person.phpm' );
+include_once( '../../lib/input.phpm' );
+include_once( '../../lib/security.phpm' );
+include_once( '../../lib/data.phpm' );
+include_once( '../../lib/output.phpm' );
+include_once( '../../inc/person.phpm' );
 
 global $config;
 
