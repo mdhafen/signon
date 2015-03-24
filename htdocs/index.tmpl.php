8976c13a592f66d0732f2f739c9dfb2b7db558c5
Michael Hafen <michael.hafen@washk12.org>
2015-03-24 15:27:08 -0600

    Changing graphics on the landing page

--- htdocs/index.tmpl.php
+++ htdocs/index.tmpl.php
@@ -6,15 +6,18 @@ include( 'doc-header.php' );
 ?>
 <div class="jumbotron">
       <div class="container">
-<h2>Welcome to Washington County School District</h2>
-  <p>Congratulations, you are eligible to create an account to use network resources.</p>
-<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+  <center>
+  <img src='img/crest.png' height='200px'>
+  </center>
       </div>
 </div>
 
 <div class="container">
 <div class="mainpage">
+<h2>Already have an WCSDsignon Account?</h2>
+<button type="button" class="btn btn-success btn-lg btn-block"><small>Click Here to</small><br>Sign In</button>
 
+<h2>Need an WCSDsignon account?</h2>
 <div class="row">
 <div class="col-md-4">
 <button type="button" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>Student</button>
@@ -24,9 +27,13 @@ include( 'doc-header.php' );
 </div>
 <div class="col-md-4">
 <button type="button" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>Public User</button>
+</div>
+</div>
+<br>
+<img src='img/WCSDsignon.jpg'>
 
-</div>
-</div>
+
+
 
 <?php include( 'doc-close.php' ); ?>
 </div>
