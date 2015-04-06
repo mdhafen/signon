07de75e1fb538a2a33629b1f480038a872355822
Michael Hafen <michael.hafen@washk12.org>
2015-04-06 13:59:41 -0600

    removing unnessessary stuff from index code

--- webroot/index.php
+++ webroot/index.php
@@ -4,23 +4,6 @@ include_once( '../lib/security.phpm' );
 include_once( '../lib/data.phpm' );
 include_once( '../lib/output.phpm' );
 
-/*
-do_ldap_connect();
-if ( ! authenticate() ) {
-	output( array(), 'login' );
-	exit;
-}
- */
-
-global $config;
 $output = array();
-
 output( $output, 'index.tmpl' );
-
-function sorter( $a, $b ) {
-	$av = empty($a['cn']) ? $a['ou'][0] : $a['cn'][0];
-	$bv = empty($b['cn']) ? $b['ou'][0] : $b['cn'][0];
-
-	return strcasecmp( $av, $bv );
-}
 ?>
