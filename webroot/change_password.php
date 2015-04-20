1a19b27d03914cbcee236054d547a61f466f27c2
Michael Hafen <michael.hafen@washk12.org>
2015-04-20 15:04:55 -0600

    Adding code to profile to change a password, and a stub for same in google
    That makes two stubs I need to fill in now.

--- webroot/change_password.php
+++ webroot/change_password.php
@@ -5,7 +5,6 @@ include_once( '../lib/data.phpm' );
 include_once( '../lib/output.phpm' );
 include_once( '../inc/person.phpm' );
 include_once( '../inc/schema.phpm' );
-include_once( '../inc/google.phpm' );
 
 authorize( 'set_password' );
 
@@ -27,21 +26,6 @@ else {
 	$output = array(
 		'object' => $object,
 	);
-
-	$password = input( 'newpassword', INPUT_STR );
-	$password2 = input( 'verifypassword', INPUT_STR );
-
-	if ( !empty($password) && !empty($password2) ) {
-		if ( $password === $password2 ) {
-			set_password( $dn, $password );
-			google_set_password( $object['mail'][0], $password );
-			$output['result'] = 'Password Set';
-		}
-		else {
-			$output['result'] = 'Passwords do not match';
-		}
-	}
-
 	output( $output, 'profile' );
 }
 ?>
