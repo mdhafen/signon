efc47a3f569cc5c9ca1ba3edd012ec70ecab162e
Michael Hafen <michael.hafen@washk12.org>
2015-04-20 16:22:21 -0600

    Adding page specifically for changing a users password from Google Accounts page
    We plan to have the Google Accounts page redirect users wanting to change
    their password to signon.  So I built a page to recieve them.

--- htdocs/change_password.php
+++ htdocs/change_password.php
@@ -4,109 +4,48 @@
 include( 'doc-head-close.php' );
 include( 'doc-header.php' );
 ?>
-<!-- Change Password Modal -->
-<div class="modal fade" id="password_modal" role="dialog" aria-labelledby="password_modal_label" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form action="profile.php" method="post" class="form-horizontal">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="password_modal_label">Change Password</h4>
-      </div>
-      <div class="modal-body">
-        <div class="container-fluid">
-          <div class="row form-group">
-            <label for="newpassword" class="control-label col-sm-4">New Password: </label>
-            <div class="col-sm-8">
-              <input id="newpassword" name="password" value="" type="password" class="form-control">
-            </div>
-          </div>
-          <div class="row form-group">
-            <label for="verifypassword" class="control-label col-sm-4">Verify Password: </label>
-            <div class="col-sm-8">
-              <input id="verifypassword" name="password2" value="" type="password" class="form-control">
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <input class="btn btn-primary" type="submit" value="Change Password">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<!-- Link to EmployeeOnline Modal -->
-<div class="modal fade" id="linkeo_modal" role="dialog" aria-labelledby="linkeo_modal_label" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form action="profile.php" method="post" class="form-horizontal">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="linkeo_modal_label">Link to EmployeeOnline</h4>
-      </div>
-      <div class="modal-body">
-        <div class="container-fluid">
-          <div class="row form-group">
-            <label for="employeeid" class="control-label col-sm-6">employeeOnline EmployeeID: </label>
-            <div class="col-sm-6">
-              <input id="employeeid" name="employeeNumber" value="<?= $data['object']['employeeNumber'][0] ?>" class="form-control">
-            </div>
-          </div>
-
-          <div class="row form-group">
-            <label for="employeessn" class="control-label col-sm-6">Social Security Number: </label>
-            <div class="col-sm-6">
-              <input id="employeessn" name="SSN" value="" class="form-control">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <input class="btn btn-primary" type="submit" value="Link to EmployeeOnline">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<h1><?= $data['object']['cn'][0] ?></h1>
 <div class="mainpage">
-
-<div class="panel panel-default panel-body">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-4 text-right">First Name: </div>
-      <div class="col-sm-8 text-left"><?= $data['object']['givenName'][0] ?></div>
-    </div>
-    <div class="row">
-      <div class="col-sm-4 text-right">Last Name: </div>
-      <div class="col-sm-8 text-left"><?= $data['object']['sn'][0] ?></div>
-    </div>
-    <div class="row">
-      <div class="col-sm-4 text-right">Username: </div>
-      <div class="col-sm-8 text-left"><?= $data['object']['uid'][0] ?></div>
-    </div>
-    <div class="row">
-      <div class="col-sm-4 text-right">E-Mail: </div>
-      <div class="col-sm-8 text-left"><?= $data['object']['mail'][0] ?></div>
-    </div>
-    <div class="row">
-      <div class="col-sm-4 text-right">Location: </div>
-      <div class="col-sm-8 text-left"><?= $data['object']['l'][0] ?></div>
+<?php if ( !empty($data['success']) ) { ?>
+<div class="alert alert-info alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+Your password has been changed.
+</div>
+<?php } else { ?>
+<h1 class="modal-title" id="password_modal_label">Change Password</h1>
+<form action="change_password.php" method="post" class="form-horizontal">
+  <div class="panel panel-default panel-body">
+    <div class="container-fluid">
+      <div class="row form-group">
+        <label for="username" class="control-label col-sm-4">Email Address: </label>
+        <div class="col-sm-8">
+          <input id="username" name="username" value="" class="form-control">
+        </div>
+      </div>
+      <div class="row form-group">
+        <label for="oldpassword" class="control-label col-sm-4">Old Password: </label>
+        <div class="col-sm-8">
+          <input id="oldpassword" name="oldpassword" value="" type="password" class="form-control">
+        </div>
+      </div>
+      <div class="row form-group">
+        <label for="newpassword" class="control-label col-sm-4">New Password: </label>
+        <div class="col-sm-8">
+          <input id="newpassword" name="password" value="" type="password" class="form-control">
+        </div>
+      </div>
+      <div class="row form-group">
+        <label for="verifypassword" class="control-label col-sm-4">Verify Password: </label>
+        <div class="col-sm-8">
+          <input id="verifypassword" name="password2" value="" type="password" class="form-control">
+        </div>
+      </div>
     </div>
   </div>
-</div>
+  <div class="panel panel-default panel-body">
+    <input class="btn btn-primary" type="submit" value="Change Password">
+  </div>
+</form>
 
-<div class="panel panel-default panel-body">
-  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#password_modal">Change Password</button>
-  <?php if ( !empty($data['object']['employeeType'][0]) && $data['object']['employeeType'][0] == 'Staff' ) { ?>
-<!--  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#linkeo_modal">Link to Employee Online</button> -->
 <?php } ?>
-</div>
-
 </div>
 <?php include( 'doc-close.php' ); ?>
--- webroot/change_password.php
+++ webroot/change_password.php
@@ -5,27 +5,102 @@ include_once( '../lib/data.phpm' );
 include_once( '../lib/output.phpm' );
 include_once( '../inc/person.phpm' );
 include_once( '../inc/schema.phpm' );
+include_once( '../inc/google.phpm' );
 
-authorize( 'set_password' );
+if ( empty($_SESSION['REFERER']) && !empty($_SERVER['HTTP_REFERER']) ) {
+	$_SESSION['REFERER'] = $_SERVER['HTTP_REFERER'];
+}
 
+$google_domain = 'washk12.org';
 $errors = array();
+$output = array();
 
 do_ldap_connect();
-$dn = $_SESSION['userid'];
-$set = ldap_quick_search( array( 'objectClass' => '*' ), array(), 0, $dn );
-$object = $set[0];
+$email = input( 'username', INPUT_HTML_NONE );
+$oldpassword = input( 'oldpassword', INPUT_STR );
+$password = input( 'newpassword', INPUT_STR );
+$password2 = input( 'verifypassword', INPUT_STR );
+$username = '';
 
-if ( ! is_person( $object ) ) {
-	$errors[] = 'PROFILE_NOT_USER';
+if ( strpos($username,'@') !== False ) {
+	if ( strripos($username,'@'.$google_domain) !== False ) {
+		$username = substr($email,0,strpos($email,'@'));
+	}
+	else {
+		$errors[] = 'WRONG_EMAIL_DOMAIN';
+	}
+}
+else {
+	//$errors[] = 'INVALID_EMAIL';
+	$username = $email;
+	$email = $username .'@'. $google_domain;
+}
+
+if ( auth_to_google( $email, $oldpassword ) ) {
+	$dn = '';
+	$set = ldap_quick_search( array( 'uid' => $username ), array() );
+	if ( empty($set) ) {
+		$user = get_user_google( $email );
+
+		if ( !empty($user) ) {
+			if ( stripos($user['orgUnitPath'],'nonusers') !== FALSE ) {
+				$errors[] = 'REGISTERING_SERVICE_ACCOUNT';
+			}
+		}
+		else if ( stripos($user['orgUnitPath'],'student') !== FALSE ) {
+			$entry['employeeType'] = 'Student';
+		}
+		else {
+			$entry['employeeType'] = 'Staff';
+		}
+
+		if ( !empty($entry['employeeType']) ) {
+			$entry['uid'] = strtolower(substr($user['primaryEmail'],0,strpos($user['primaryEmail'],'@')));
+			$entry['sn'] = $user['name']['familyName'];
+			$entry['givenName'] = $user['name']['givenName'];
+			$entry['cn'] = $user['name']['fullName'];
+			$entry['mail'] = strtolower($user['primaryEmail']);
+			$entry['l'] = google_org_to_loc( $user['orgUnitPath'] );
+			$ou = google_org_to_ou( $user['orgUnitPath'], $entry['l'] );
+			if ( !empty($ou) && !empty($entry['uid']) ) {
+				$dn = 'uid='. ldap_escape($entry['uid'],'',LDAP_ESCAPE_DN) .','. $ou;
+			}
+
+			if ( !empty($dn) ) {
+				$entry['sambaSID'] = ldap_get_next_SID();
+				do_ldap_add( $dn, $entry );
+			}
+		}
+	}
+	else {
+		$object = $set[0];
+		$dn = $object['dn'];
+	}
+
+	if ( ! empty($dn) ) {
+		if ( $password === $password2 && strlen($password) >=8 ) {
+			set_password( $dn, $password );
+			google_set_password( $email, $password );
+			$output['success'] = true;
+		}
+		else {
+			$errors[] = 'PASSWORDS_NO_MATCH';
+		}
+	}
+	else {
+		$errors[] = 'USER_NOT_FOUND';
+	}
 }
 
 if ( ! empty($errors) ) {
 	error( $errors );
 }
 else {
-	$output = array(
-		'object' => $object,
-	);
-	output( $output, 'profile' );
+	if ( !empty($_SESSION['REFERER']) && stripos($_SESSION['REFERER'],'https://myaccount.google.com') !== False ) {
+		redirect( 'https://myaccount.google.com' );
+	}
+	else {
+		output( $output, 'change_password' );
+	}
 }
 ?>
