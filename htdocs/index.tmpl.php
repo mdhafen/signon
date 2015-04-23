8f1f644ee8459e24f7991b03b4b3e2f5a3034b25
Michael Hafen <michael.hafen@washk12.org>
2015-04-23 11:52:24 -0600

    Also change the links

--- htdocs/index.tmpl.php
+++ htdocs/index.tmpl.php
@@ -20,13 +20,13 @@ include( 'doc-header.php' );
 <h2>Need an WCSDsignon account?</h2>
 <div class="row">
 <div class="col-md-4">
-<a href="create.php?op=Student" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>Student</a>
+<a href="create/create.php?op=Student" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>Student</a>
 </div>
 <div class="col-md-4">
-<a href="create.php?op=Employee" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>WCSD Employee</a>
+<a href="create/create.php?op=Employee" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>WCSD Employee</a>
 </div>
 <div class="col-md-4">
-<a href="create.php?op=Guest" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>Visitor</a>
+<a href="create/create.php?op=Guest" class="btn btn-primary btn-lg btn-block"><small>I am a</small><br>Visitor</a>
 </div>
 </div>
 <br>
