7d2af4224ac44e0a287af30816c6eb9fae2773c0
Michael Hafen <michael.hafen@washk12.org>
2015-09-15 12:11:41 -0600

    Capture client mac address for later

--- webroot/index.php
+++ webroot/index.php
@@ -4,6 +4,11 @@ include_once( '../lib/security.phpm' );
 include_once( '../lib/data.phpm' );
 include_once( '../lib/output.phpm' );
 
+$c_mac = input( 'client_mac', INPUT_HTML_NONE );
+if ( !empty($c_mac) ) {
+  $_SESSION['client_mac'] = $c_mac;
+}
+
 $output = array();
 output( $output, 'index.tmpl' );
 ?>
