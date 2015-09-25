4b1762ce7d6426ad78844e7fe12c6e2ab0b8a741
Michael Hafen <michael.hafen@washk12.org>
2015-09-25 16:35:51 -0600

    Playing with cancel button on user profile change password modal
    I changed the button to an anchor, so even with btn-default it is not the
    default action.  Problem solved.

--- htdocs/profile.php
+++ htdocs/profile.php
@@ -30,7 +30,7 @@ include( 'doc-header.php' );
         </div>
         <div class="modal-footer">
           <input class="btn btn-primary" type="submit" value="Change Password">
-          <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+          <a href="#" class="btn btn-default" data-dismiss="modal">Cancel</a>
         </div>
       </div>
       </form>
