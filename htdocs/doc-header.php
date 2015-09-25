5c653169d351e2ff453731d15f7e904bcf42d516
Michael Hafen <michael.hafen@washk12.org>
2015-09-25 16:14:47 -0600

    Change the Brand and page title to WCSDsignon System

--- htdocs/doc-header.php
+++ htdocs/doc-header.php
@@ -8,22 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">WCSDsignon Authentication System</a>
-        </div>
-<?php if ( ! empty($data['_session']['username']) ) { ?>
-        <div class="nav navbar-right dropdown">
-          <a class="navbar-btn btn dropdown-toggle" data-toggle="dropdown" href="#">
-          <span class="glyphicon glyphicon-user"> <?= $data['_session']['username'] ?></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li><a href="<?= $data['_config']['base_url'] ?>profile.php">Profile</a></li>
-            <li class="divider"></li>
-            <li><a href="<?= $data['_config']['base_url'] ?>?_logout=1">Sign Out</a></li>
-          </ul>
-<?php } else { ?>
-	<div class="nav navbar-right">
-          <a class="navbar-btn btn" href="<?= $data['_config']['base_url'] ?>profile.php">Sign In</a>
-<?php } ?>
+          <a class="navbar-brand" href="<?= $data['_config']['base_url'] ?>">WCSDsignon System</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
          <ul class="nav navbar-nav">
@@ -34,6 +19,21 @@
 <?php } ?>
 
          </ul>
+<?php if ( ! empty($data['_session']['username']) ) { ?>
+         <div class="nav navbar-right dropdown">
+          <a class="navbar-btn btn dropdown-toggle" data-toggle="dropdown" href="#">
+          <span class="glyphicon glyphicon-user"> <?= $data['_session']['username'] ?></span>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a href="<?= $data['_config']['base_url'] ?>profile.php">Profile</a></li>
+            <li class="divider"></li>
+            <li><a href="<?= $data['_config']['base_url'] ?>?_logout=1">Sign Out</a></li>
+          </ul>
+<?php } else { ?>
+	 <div class="nav navbar-right">
+           <a class="navbar-btn btn" href="<?= $data['_config']['base_url'] ?>profile.php">Sign In</a>
+<?php } ?>
+         </div>
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
