311022ab890b65d3e2b4f7f4b0e620a3af9be419
Michael Hafen <michael.hafen@washk12.org>
2015-12-08 12:14:29 -0700

    Make the username display on the user registration bigger
    Hopefully that will stand out enough to avoid confusion.

--- htdocs/create/create.php
+++ htdocs/create/create.php
@@ -26,7 +26,7 @@ There was an error! <?= $data['result'] ?>
 <div class="alert alert-info alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 <?php if ( !empty($data['username']) ) { ?>
-  <div>Welcome <?= $data['username'] ?></div>
+  <h2>Welcome <?= $data['username'] ?></h2>
 <?php } ?>
   Please enter your Washington County School District EMail password below.
 </div>
