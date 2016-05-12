284baf8c071f92a6ad74ebda35ff9bc5b59d560c
Michael Hafen <michael.hafen@washk12.org>
2016-05-12 10:45:08 -0600

    Move jquery from hosted to local because of the walled garden

--- htdocs/doc-open.php
+++ htdocs/doc-open.php
@@ -7,5 +7,5 @@
 <meta name="description" content="WCSDsignon">
 <meta name="author" content="WCSD">
 <link rel="icon" href="<?= $data['_config']['base_url'] ?>favicon.ico">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="<?= $data['_config']['base_url'] ?>js/jquery.min.js"></script>
 <script src="<?= $data['_config']['base_url'] ?>js/bootstrap.min.js"></script>
