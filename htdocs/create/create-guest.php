db2f368a08b6a36b1f5dea7f444499fbe65e3a85
Michael Hafen <michael.hafen@washk12.org>
2016-07-05 12:42:18 -0600

    Specify Cell Phone on the guest signup page.

--- htdocs/create/create-guest.php
+++ htdocs/create/create-guest.php
@@ -14,7 +14,7 @@ if ( !empty($data['result']) ) {
   if ( empty($data['error']) ) { ?>
 <div class="alert alert-info alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-Your account has been created.  Please switch to the WCSDaccess network and use your phone number, exactly as you entered it on the registration page, to login.  Your password will be sent you by in an SMS.
+Your account has been created.  Please switch to the WCSDaccess network and use your cell phone number, exactly as you entered it on the registration page, to login.  Your password will be sent you by in an SMS.
 </div>
 <?php
   } else { /* error */ ?>
@@ -33,8 +33,8 @@ There was an error! <?= $data['result'] ?>
   <div class="container-fluid">
     <form action="create.php" method="post" class="form-horizontal">
     <div class="row form-group">
-      <label for="mobile" class="col-sm-4 control-label">Phone Number: </label>
-      <div class="col-sm-8"><input id="mobile" name="mobile" value="" class="form-control" type="tel" required="required" placeholder="xxx-xxx-xxxx" pattern="[0-9]{3}-?[0-9]{3}-?[0-9]{4}" title="A phone number with the three digit area code, three digit prefix, and four digits.  Dashes optional"></div>
+      <label for="mobile" class="col-sm-4 control-label">Cell Phone Number: </label>
+      <div class="col-sm-8"><input id="mobile" name="mobile" value="" class="form-control" type="tel" required="required" placeholder="xxx-xxx-xxxx" pattern="[0-9]{3}-?[0-9]{3}-?[0-9]{4}" title="A cell phone number with the three digit area code, three digit prefix, and four digits.  Dashes optional"></div>
     </div>
 
     <div class="row form-group">
