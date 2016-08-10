f6dc768aab82b42beafed35f40a78c483b1aea4e
Michael Hafen <michael.hafen@washk12.org>
2016-08-10 11:26:01 -0600

    Fixing registration error message and renaming file

--- webroot/errors.php
+++ webroot/errors.php
@@ -10,7 +10,7 @@ $message = "";
 if ( $_SERVER['REDIRECT_STATUS'] == "403" ) {
     $message = "Access Denied.";
     $url_base = substr( $config['base_url'], stripos($config['base_url'],'/',8) );
-    if ( stripos($_SERVER['REDIRECT_URL'],$url_base.'/create') === 0 ) {
+    if ( stripos($_SERVER['REDIRECT_URL'],$url_base.'create') === 0 ) {
         $message .= "  Registration is only allowed on School District networks.";
     }
 }
