5dc5f304b4e2284ebc862b6f2ddc3e87e035a7f2
Michael Hafen <michael.hafen@washk12.org>
2016-11-08 13:03:37 -0700

    Adding password entropy to the change_password page and the logs

--- htdocs/change_password.php
+++ htdocs/change_password.php
@@ -19,7 +19,8 @@ include( 'doc-header.php' );
       <div class="row form-group">
         <label for="newpassword" class="control-label col-sm-4">New Password: </label>
         <div class="col-sm-8">
-          <input id="newpassword" name="password" value="" type="password" class="form-control">
+          <input id="newpassword" name="password" value="" type="password" class="form-control" onkeyup="CheckEntropy(this.value,'EntropyMeter')">
+          Entropy: <span id="EntropyMeter"></span>
         </div>
       </div>
       <div class="row form-group">
@@ -28,6 +29,9 @@ include( 'doc-header.php' );
           <input id="verifypassword" name="password2" value="" type="password" class="form-control">
         </div>
       </div>
+    </div>
+    <div>
+      Washington County School District recommends passwords have an entropy value of at least 70.
     </div>
     <div>
       This password will be used on the following services:
@@ -44,6 +48,37 @@ include( 'doc-header.php' );
   </div>
 </form>
 
+<script>
+  Math.log2 = Math.log2 || function(x) Math.log(x)*Math.LOG2E;};
+  function CheckEntropy(value,report_el_id) {
+    var guideline = 70;
+    var char_sets = {
+      lower: { reg: /[a-z]/g, entropy:26 },
+      upper: { reg: /[A-Z]/g, entropy:26 },
+      numbers: { reg: /[0-9]/g, entropy:10 },
+      punct: { reg: /[,\.\?'";:!@#\$%\^&\*\-_]/g, entropy:17 }, // '
+      symbols: { reg: /[`~\(\)+=\{\}\|\[\]\\/<>]/g, entropy:15 } // `
+    };
+    var el = document.getElementById(report_el_id);
+    var length = value.length;
+    var entropy = 0;
+    for ( var set in char_sets ) {
+        if ( char_sets.hasOwnProperty(set) && char_sets[set].reg.test(value) ) {
+            value = value.replace( char_sets[set].reg, "" );
+            entropy += char_sets[set].entropy;
+        }
+    }
+    if ( entropy ) { entropy = Math.floor( Math.log2(entropy) ) * length; }
+    while ( el.firstChild ) { el.removeChild(el.firstChild); }
+    el.appendChild( document.createTextNode(entropy) );
+    if ( entropy >= guideline ) {
+        $(el).removeClass("bg-warning").addClass("bg-success");
+    }
+    else {
+        $(el).removeClass("bg-success").addClass("bg-warning");
+    }
+  }
+</script>
 <?php } ?>
 </div>
 </div>
