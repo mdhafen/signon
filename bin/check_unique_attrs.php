78d944ae5466420f315e9b97562b00240a54cd7f
Michael Hafen <michael.hafen@washk12.org>
2017-08-28 13:44:17 -0600

    in check_unique_attrs.php - initialize the array elements
    to avoid the undefined index warnings.

--- bin/check_unique_attrs.php
+++ bin/check_unique_attrs.php
@@ -9,6 +9,12 @@ $ldap = new LDAP_Wrapper();
 $attrs = $ldap->quick_search( '(&(objectClass=sambaSamAccount)(!(employeeType=Guest)))', $unique_attrs );
 foreach ( $attrs as $user ) {
     foreach ( $unique_attrs as $attr ) {
+        if ( empty($matches[$attr]) ) {
+            $matches[$attr] = array();
+        }
+        if ( empty($matches[$attr][ $user[$attr][0] ]) ) {
+            $matches[$attr][ $user[$attr][0] ] = 0;
+        }
         $matches[$attr][ $user[$attr][0] ]++;
     }
 }
