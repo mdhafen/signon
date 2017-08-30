537775b8ed6e4dac1086a3a06dbe4f9a2c4dc86b
Michael Hafen <michael.hafen@washk12.org>
2017-08-30 12:29:26 -0600

    Add a script to remove empty ou's, and a member function to get ldap errors.

---
+++ bin/remove_empty_ous.php
@@ -0,0 +1,25 @@
+<?php
+include_once( '../lib/data.phpm' );
+
+$ldap = new LDAP_Wrapper();
+$ous = $ldap->quick_search( '(objectClass=organizationalUnit)', array() );
+$dns = array();
+
+foreach ( $ous as $ou ) {
+    $dn = $ou['dn'];
+    $children = $ldap->quick_search( array( 'objectClass' => '*' ), array(), 1, $dn );
+    if ( ! count($children) ) {
+        $dns[] = $dn;
+    }
+}
+
+foreach ( $dns as $dn ) {
+    print "$dn";
+    $result = $ldap->do_delete( $dn );
+    if ( ! $result ) {
+        print $ldap->get_error();
+    }
+    print "\n";
+}
+
+?>
