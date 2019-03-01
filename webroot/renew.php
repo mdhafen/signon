64307995ece384ea20988184eaa84e629e4c284c
Michael Hafen <michael.hafen@washk12.org>
2019-03-01 11:58:04 -0700

    Add Path_Info parsing to /renew.php and the guest signature renew sms
    So that (hopefully) the phone will make the whole url a link.

--- webroot/renew.php
+++ webroot/renew.php
@@ -8,6 +8,13 @@ include_once( '../inc/person.phpm' );
 $token = input( 't', INPUT_HTML_NONE );
 $submit = input( 'submit', INPUT_STR );
 
+if ( empty($token) && !empty($_SERVER['PATH_INFO']) ) {
+    $paths = explode( '/', $_SERVER['PATH_INFO'] );
+    if ( !empty($paths) ) {
+        $token = !empty($paths[0]) ? $paths[0] : $paths[1];
+    }
+}
+
 $output = array('token'=>$token);
 
 if ( !empty($token) && !empty($submit) ) {
