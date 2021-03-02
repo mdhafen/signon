5eb957078d9cdec0bab597f91cb50253d367a633
Michael Hafen <michael.hafen@washk12.org>
2021-03-02 15:22:06 -0700

    Add script to set default password for students
    doesn't change their active passwords, just sets their default if it isn't
    set.

--- bin/set_default_password.php
+++ bin/set_default_password.php
@@ -31,15 +31,16 @@ foreach ( $users as $user ) {
     print "Could not compute old default password for ". $user['dn'] ."\n";
     continue;
   }
-  $password = get_default_password($user['dn']);
-  if ( empty($password) ) {
-    print "No default password for ". $user['dn'] ."\n";
+  if ( empty($user['employeeType']) || $user['employeeType'][0] != 'Student' ) {
+    // limit to students for now.
     continue;
   }
-
-  if ( @$ldap->do_connect('core',$user['dn'],$password) ) {
-    $change_users[] = $user;
+  $password = get_default_password($user['dn']);
+  if ( ! empty($password) ) {
+    print "default password already set for ". $user['dn'] ."\n";
+    continue;
   }
+  $change_users[] = $user;
 }
 unset($users);
 
@@ -48,17 +49,20 @@ $ldap->do_connect( 'core', $ldap->config['userdn'], $ldap->config['passwd'] );
 print "Continuing\n";
 
 foreach ( $change_users as $user ) {
+  // old password scheme
+  $password = mb_strtolower(mb_substr($user['givenName'][0],0,1))
+	 . mb_strtolower(mb_substr($user['sn'][0],0,1))
+	 . $user['employeeNumber'][0];
+
+/*
   $password = create_password();
   // turn the three-word passphrase into a two-word passphrase
   $password = substr( $password, 0, strrpos($s,'-') );
+ */
 
-  print "Changing password for ". $user['dn'] ."\n";
+  print "Setting default password for ". $user['dn'] ."\n";
 
   set_default_password( $user['dn'], $password );
-  if ( !empty($user['employeeType'][0]) && strtolower(strstr($email,'@')) == '@'.$GOOGLE_DOMAIN ) {
-    $result = google_set_password( $user['mail'][0], $password );
-  }
-  $result = set_password( $ldap, $user['dn'], $password );
 }
 
 ?>
