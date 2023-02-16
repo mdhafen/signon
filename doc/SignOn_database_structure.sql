8d7d807ca33dd616028474847cfcec4adf41b00a
Michael Hafen <michael.hafen@washk12.org>
2023-02-16 11:16:43 -0700

    Clean up SignOn database file

--- doc/SignOn_database_structure.sql
+++ doc/SignOn_database_structure.sql
@@ -1,27 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
---
--- Host: 205.126.10.160    Database: SignOn
--- ------------------------------------------------------
--- Server version	5.7.26-0ubuntu0.18.04.1-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `attribute_changes`
 --
 
 DROP TABLE IF EXISTS `attribute_changes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attribute_changes` (
   `dn` varchar(256) NOT NULL,
   `attr` varchar(28) NOT NULL,
@@ -31,15 +12,12 @@ CREATE TABLE `attribute_changes` (
   `timestamp` datetime DEFAULT NULL,
   PRIMARY KEY (`attr`,`dn`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `user_locks`
 --
 
 DROP TABLE IF EXISTS `user_locks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_locks` (
   `dn` varchar(256) NOT NULL,
   `passwd` mediumtext NULL,
@@ -47,15 +25,12 @@ CREATE TABLE `user_locks` (
   `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`dn`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `guest_signatures`
 --
 
 DROP TABLE IF EXISTS `guest_signatures`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `guest_signatures` (
   `guest_uid` varchar(256) NOT NULL,
   `aup_sent` datetime DEFAULT NULL,
@@ -64,42 +39,26 @@ CREATE TABLE `guest_signatures` (
   `guest_token` varchar(96) DEFAULT NULL,
   PRIMARY KEY (`guest_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `user_default_password`
 --
 
 DROP TABLE IF EXISTS `user_default_password`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_default_password` (
   uid varchar(40) NOT NULL,
   default_password varchar(39) DEFAULT NULL,
   timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (uid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Table structure for table `pwned_passwords`
 --
 
 DROP TABLE IF EXISTS `pwned_passwords`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pwned_passwords` (
   `hash` varchar(40) NOT NULL,
   `times_seen` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-05-06 13:14:49
