2a64eb860dbc01e6389cfeb3af211d2fca0c6942
Michael Hafen <michael.hafen@washk12.org>
2024-02-21 11:58:13 -0700

    encode error message in view/error.tmpl.php

--- view/error.tmpl.php
+++ view/error.tmpl.php
@@ -11,7 +11,7 @@ include( 'doc-header.php' );
 <?php
 foreach ( $data['errors'] as $error ) {
 ?>
-<div class='important'><?= $error ?></div>
+<div class='important'><?= htmlspecialchars($error,ENT_QUOTES|ENT_HTML5|ENT_SUBSTITUTE) ?></div>
 <?php if ( $error == 'PASS_TOO_COMMON' ) { ?>
 <p><span class="big">Password compromised, you can not use this password.</span><br>  This password has been seen <?= $data['error_times'] ?> times before.  <span class="small">This password has previously appeared in a data breach and should never be used.  If you've ever used it anywhere before, you should change it as soon as possible.</span></p>
 <?php } ?>
