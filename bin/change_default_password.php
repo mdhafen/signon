7b654578afdf148cca39e10e7c7aebdf8aad17cd
Michael Hafen <michael.hafen@washk12.org>
2025-04-23 11:25:04 -0600

    Add sight_words dictionary for generating passwords
    Only use sight_words dictionary for elementary school students.
    Tweak bin/change_default_password.php to more thouroughly change default
    passwords.

--- bin/change_default_password.php
+++ bin/change_default_password.php
@@ -23,7 +23,8 @@ if ( !empty($argv[1]) ) {
   $uid = substr($email,0,strpos($email,'@'));
   $users = $ldap->quick_search( "(|(mail=$email)(uid=$uid))" , array() );
 } else {
-  $users = $ldap->quick_search( '(&(|(employeeType=Staff)(employeeType=Student)(employeeType=Other))(objectClass=inetOrgPerson))' , array() );
+  // FIXME limit to students for now
+  $users = $ldap->quick_search( '(&(employeeType=Student)(objectClass=inetOrgPerson))' , array() );
 }
 
 $change_users = array();
@@ -31,26 +32,38 @@ foreach ( $users as $user ) {
   if ( $user['employeeType'][0] == 'Student' ) {
     if ( empty($user['givenName']) || empty($user['sn']) || empty($user['employeeNumber']) ) {
       print "Could not compute student old default password for ". $user['dn'] ."\n";
-      continue;
+      $password = '';
     }
-    $password = mb_strtolower(mb_substr($user['givenName'][0],0,1))
-       . mb_strtolower(mb_substr($user['sn'][0],0,1))
-       . $user['employeeNumber'][0];
-    if ( @$ldap->do_connect('core',$user['dn'],$password) ) {
-      $change_users[] = $user;
-      continue;
+    else {
+      $password = mb_strtolower(mb_substr($user['givenName'][0],0,1))
+         . mb_strtolower(mb_substr($user['sn'][0],0,1))
+         . $user['employeeNumber'][0];
+      if ( @$ldap->do_connect('core',$user['dn'],$password) ) {
+        $change_users[] = $user;
+        continue;
+      }
     }
   }
 
-  $password = get_default_password($user['uid'][0]);
-  if ( empty($password) ) {
-    print "No default password for ". $user['uid'][0] ."\n";
+  $def_password = get_default_password($user['uid'][0]);
+  if ( empty($def_password) || $def_password == $password ) {
+    if ( empty($def_password) ) {
+      print "No default password for ". $user['uid'][0] .", setting one.\n";
+    }
+    else {
+      print "Bad default password for ". $user['uid'][0] .", changing it.\n";
+    }
+    $def_password = generate_default_password($user);
+    set_default_password( $user['uid'][0], $def_password );
     continue;
   }
 
-  if ( @$ldap->do_connect('core',$user['dn'],$password) ) {
+  // FIXME don't change a good default password at this point
+  /*
+  if ( @$ldap->do_connect('core',$user['dn'],$def_password) ) {
     $change_users[] = $user;
   }
+  */
 }
 unset($users);
 
@@ -59,10 +72,10 @@ $ldap->do_connect( 'core', $ldap->config['userdn'], $ldap->config['passwd'] );
 print "Continuing\n";
 
 foreach ( $change_users as $user ) {
+  print "Changing password and default for ". $user['dn'] ."\n";
+
   $password = generate_default_password($user);
   set_default_password( $user['uid'][0], $password );
-
-  print "Changing password for ". $user['dn'] ."\n";
 
   if ( strtolower(strstr($user['mail'][0],'@')) == '@'.$GOOGLE_DOMAIN ) {
     $result = google_set_password( $user['mail'][0], $password );
