42f341cc0bdf84064f302fe67a95716b232e1219
Michael Hafen <michael.hafen@washk12.org>
2026-07-22 16:38:34 -0600

    bin/remove_empty_ous.php - scan longest dn's first so only 1 pass is needed
    While considering an ou's children, check if the child is an ou, and has it's
    own children.  If it's an ou with no children assume it's going to be removed,
    and if this ou doesn't have any children that are staying, then it can be
    removed too.
    This way all the ou's can be removed in one pass, instead of having to keep
    running this script until nothing is removed.

--- bin/remove_empty_ous.php
+++ bin/remove_empty_ous.php
@@ -4,6 +4,7 @@ include_once( '../lib/data.phpm' );
 $ldap = new LDAP_Wrapper();
 $ous = $ldap->quick_search( '(objectClass=organizationalUnit)', array() );
 $dns = array();
+uasort( $ous, function( $a, $b ) { return mb_strlen($b['dn']) <=> mb_strlen($a['dn']); } );
 
 foreach ( $ous as $ou ) {
     $dn = $ou['dn'];
@@ -13,17 +14,24 @@ foreach ( $ous as $ou ) {
     if ( stripos($ou['ou'][0],'ou=wcsd,ou=technology') !== false ) {
         continue;
     }
+    $dns[$dn] = 0;
     $children = $ldap->quick_search( '(objectClass=*)', array(), 1, $dn );
-    if ( ! count($children) ) {
-        $dns[] = $dn;
+    foreach ( $children as $child ) {
+        if ( ! array_key_exists($child['dn'],$dns) ||  $dns[$child['dn']] > 0 ) {
+            $dns[$dn]++;
+        }
     }
 }
 
-foreach ( $dns as $dn ) {
+foreach ( $dns as $dn => $children ) {
+    if ( $children > 0 ) {
+        continue;
+    }
+
     print "$dn";
     $result = $ldap->do_delete( $dn );
     if ( $result ) {
-        print $result;
+        print " (Error:$result)";
     }
     print "\n";
 }
